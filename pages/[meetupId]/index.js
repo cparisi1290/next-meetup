7c2872a36516f9c9f07e2df7f4d656c3447e3dfc
Cassandra <cparisi1290@gmail.com>
2021-08-02 14:08:04 -0500

    change name of folder

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://www.busytourist.com/wp-content/uploads/2019/07/Albuquerque-Balloon-Fiesta.jpg.webp"
+      title="A First Meetup"
+      address="Some street, Some city"
+      description="This is a Hot Air Balloon Festival Meetup"
+    />
+  );
+}
+
+export default MeetupDetails;
